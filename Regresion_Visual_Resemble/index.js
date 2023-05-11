b41cd6b8ba40a53860410f33436a1cc82ca3d7e5
Sebastian Mascareno Dagnino <s.mascareno@uniandes.edu.co>
2023-05-11 16:06:54 -0700

    Se genera script para hacer comparacion de screenshots

---
+++ Regresion_Visual_Resemble/index.js
@@ -0,0 +1,50 @@
+
+const fs = require('fs');
+const path = require('path');
+const compareImages = require('resemblejs/compareImages');
+const tests = ['Pages_DeletePage', 'Pages_NewPage'];
+
+let folder1;
+let folder2;
+let options = {};
+let files1;
+let files2;
+
+async function executeTest(){
+
+    for(let i = 0; i < tests.length; i++){
+
+        const test = tests[i];
+        folder1 = `../Capturas_Ghost_V4.44/${test}_4.44`;
+        folder2 = `../Capturas_Ghost_V5.40.2/Cypress/${test}`;
+        
+        files1 = fs.readdirSync(folder1);
+        files2 = fs.readdirSync(folder2);
+
+        let resultsFolder = `results`;
+        if (!fs.existsSync(resultsFolder)){
+            fs.mkdirSync(resultsFolder);
+        }
+
+        resultsFolder = `results/${test}`;
+        if (!fs.existsSync(resultsFolder)){
+            fs.mkdirSync(resultsFolder);
+        }
+
+        for (const file1 of files1) {
+            const file2 = files2.find((f) => f === file1);
+            if (file2) {
+                const data = await compareImages(
+                    fs.readFileSync(path.join(folder1, file1)),
+                    fs.readFileSync(path.join(folder2, file2)),
+                    options
+                );
+                fs.writeFileSync(`results/${test}/${file1}_compare.png`, data.getBuffer());
+            }
+        }
+    }
+
+    return 'completado'
+}
+
+(async () => console.log(await executeTest()))();
